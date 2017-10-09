46ca259962313e3f61df5be3606a56564efeb900
Alan Hong <xiupan@gmail.com>
2017-10-09 12:31:26 -0500

    Created Store and Reducer framework with Redux.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux'
+import {createStore, combineReducers, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const imagesReducer = (state = [], action) => {
+  return state
+}
+
+const searchQueryReducer = (state = "", action) => {
+  return state
+}
+
+const allReducers = combineReducers({
+  images: imagesReducer,
+  searchQuery: searchQueryReducer
+})
+
+const store = createStore(allReducers, applyMiddleware(thunk))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
